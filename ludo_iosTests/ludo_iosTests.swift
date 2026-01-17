ed20de7af0b5e219f2e229a95fb18df433a69c97
bksourabh <bksourabh@gmail.com>
2026-01-17 21:49:52 +1100

    Initial Commit

---
+++ ludo_iosTests/ludo_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  ludo_iosTests.swift
+//  ludo_iosTests
+//
+//  Created by Sourabh Mazumder on 17/1/2026.
+//
+
+import Testing
+@testable import ludo_ios
+
+struct ludo_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
